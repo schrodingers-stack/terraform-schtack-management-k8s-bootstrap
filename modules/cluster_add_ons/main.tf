9ac3aad9b28b348b3f93600278b06020c0fec086
Gonçalo Heleno <goncalocheleno@atomp.eu>
2024-11-22 13:08:41 +0100

    chore: remove TODOs that have moved to issues

--- modules/cluster_add_ons/main.tf
+++ modules/cluster_add_ons/main.tf
@@ -1,11 +1,3 @@
-# TODO Add a way to create a secret for repository credentials
-
-# TODO Add a way to add a private repo to Argo CD
-
-# TODO Add a way to pass some common values to all the applications
-
-
-
 resource "helm_release" "cluster_add_ons" {
   name      = "management-cluster-add-ons"
   chart     = "${path.module}/chart"
